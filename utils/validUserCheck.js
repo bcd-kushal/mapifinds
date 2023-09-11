92afa4e20477fd4e4b22f4685f588392f094bee1
Kushal-Kumar02 <am.kushal02@gmail.com>
2023-09-11 23:54:44 +0530

    commit

--- utils/validUserCheck.js
+++ utils/validUserCheck.js
@@ -6,15 +6,13 @@ const checkValidityOfNewUser = (req) => {
     //user.pfp may or may not exist, so dont fetch it
     if (req.body.userName && req.body.firstName && req.body.lastName && req.body.emailID && req.body.passw && req.body.address && req.body.addressCoOrd)
         return true
-    else
-        return false
+    return false
 }
 
 const checkValidityOfUserDetails = (req) => {
     if (req.body.firstName !== '' && req.body.lastName !== '' && req.body.address && req.body.addressCoOrd)
         return true
-    else
-        return false
+    return false
 }
 
 module.exports = { checkValidityOfNewUser, checkValidityOfUserDetails }
